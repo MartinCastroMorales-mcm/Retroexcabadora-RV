dc5513b75cd47fdddf459fbb5078a979de689bd1
MartinCastroMorales-mcm <mcm.martincastromorales@gmail.com>
2025-06-18 19:00:58 -0400

    Agregar configuracion del manurio

--- Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
+++ Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
@@ -8,6 +8,8 @@ public class RetroExcabadora : MonoBehaviour
     public InputActionProperty rightAction;
     public GameObject player;
     public GameObject targetInVehicle;
+    public GameObject playerCamera;
+    //public CharacterController characterController;
     bool isInVehicle = false;
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,7 @@ public class RetroExcabadora : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rightAction.action.ReadValue<float>());
+        //TODO: Solo activar cuando se toque la puerta
         if (rightAction.action.ReadValue<float>() == 1)
         {
             this.EnterExitVehicle();
@@ -30,12 +32,25 @@ public class RetroExcabadora : MonoBehaviour
         if (isInVehicle)
         {
             Debug.Log("Exit the vehicle");
+            //characterController.height = 5;
         }
         else
         {
             Debug.Log("Enter the vehicle");
-            player.transform.position = targetInVehicle.transform.position;
+            //characterController.height = 2;
+            //playerCamera.transform.Rotate(7.9f, -142.6f, 0f);
+            //playerCamera.transform.rotation = Quaternion.Euler(7.9f, -142.6f, 0f);
+            //player.transform.Rotate(-3.847f, 374.079f, 0f, Space.World);
+            //playerCamera.transform.Rotate(0f, 0f, 0f, Space.World);
+            player.transform.position = targetInVehicle.transform.position + new Vector3(0f, -0.5f, 0f);
 
         }
     }
 }
+//-3.847
+//374.079
+
+
+
+//7.8
+//-142.6
