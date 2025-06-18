69b868e70ea8d34398e16a4a128a802e47635de9
Martín Castro Morales <mcm.MartinCastroMorales@gmail.com>
2025-06-18 20:23:02 +0000

    Martin (#3)
    * add: retroexcavadora
    
    - prefab de retroexcavadora creado
    
    * update: retroexcavadora
    
    - Retroexcavadora creada.
    - Se hizo un prefab con la pala de la retroexcavadora.
    - Se actualizó el prefab para que las ventanas sean independientes del resto del cuerpo
    
    * agregar assets del entorno
    
    * add transperancy in windows
    
    * manurio y joystick
    
    * agregar mas modelos
    
    * Agregar forma de entrar al vehiculo(tmp)
    
    ---------
    
    Co-authored-by: Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>

--- Assets/ProyectoRetroExcabadora/mScripts/AnimateHandOnInput.cs
+++ Assets/ProyectoRetroExcabadora/mScripts/AnimateHandOnInput.cs
@@ -8,6 +8,8 @@ public class AnimateHandOnInput : MonoBehaviour
     public Animator handAnimator;
     public InputActionProperty pellizcoAnimationAction;
     public InputActionProperty agarreAnimationAction;
+
+    
     // Start is called before the first frame update
     void Start()
     {
