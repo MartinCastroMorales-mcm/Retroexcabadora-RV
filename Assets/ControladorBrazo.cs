489ba1e05f9b9cf3f2962f84124a0e006aaaf730
Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2025-06-18 20:34:36 -0400

    Leonardo (#4)
    * add: retroexcavadora
    
    - prefab de retroexcavadora creado
    
    * update: retroexcavadora
    
    - Retroexcavadora creada.
    - Se hizo un prefab con la pala de la retroexcavadora.
    - Se actualizó el prefab para que las ventanas sean independientes del resto del cuerpo
    
    * update: brazo base
    
    - El brazo base de la retroexcavadora se puede mover de izquiera a derecha.
    
    * Pendiente: Si se mueve de su lugar esta no puede hacer nada.
    
    * update: comandos actualizados
    
    - La rotación se le encargó a al joystick izquierdo solamente.
    Pendiente: todo lo demás.

---
+++ Assets/ControladorBrazo.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControladorBrazo : MonoBehaviour
+{
+    [SerializeField] private float speed = 30f;
+    [SerializeField] private InputActionReference moverBrazoIzquierda; // usar control izquierdo
+    [SerializeField] private InputActionReference moverBrazoDerecha;   // usar control izquierdo
+
+    [SerializeField] private float minY = 130f;  // Limite minimo en grados
+    [SerializeField] private float maxY = 170f;  // Limite maximo en grados
+
+    private void OnEnable()
+    {
+        moverBrazoIzquierda.action.Enable();
+        moverBrazoDerecha.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moverBrazoIzquierda.action.Disable();
+        moverBrazoDerecha.action.Disable();
+    }
+
+    void Update()
+    {
+        float direccion = 0f;
+
+        if (moverBrazoIzquierda.action.IsPressed())
+            direccion = 1f;
+        else if (moverBrazoDerecha.action.IsPressed())
+            direccion = -1f;
+
+        float nuevaRotacionY = transform.eulerAngles.y + direccion * speed * Time.deltaTime;
+
+        // Ajustar si pasa de 360°
+        nuevaRotacionY = (nuevaRotacionY + 360f) % 360f;
+
+        // Verificar que esté dentro de los límites
+        if (nuevaRotacionY >= minY && nuevaRotacionY <= maxY)
+        {
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                nuevaRotacionY,
+                transform.eulerAngles.z
+            );
+        }
+    }
+}
