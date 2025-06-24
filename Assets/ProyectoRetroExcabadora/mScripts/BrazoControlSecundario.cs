534749c9e497cd05c77f4dc456c2bcdfe2e76381
Martín Castro Morales <mcm.MartinCastroMorales@gmail.com>
2025-06-24 17:28:24 -0400

    Leonardo (#7)
    * add: retroexcavadora
    
    - prefab de retroexcavadora creado
    
    * update: retroexcavadora
    
    - Retroexcavadora creada.
    - Se hizo un prefab con la pala de la retroexcavadora.
    - Se actualizó el prefab para que las ventanas sean independientes del resto del cuerpo
    
    * update: brazo base
    
    - El brazo base de la retroexcavadora se puede mover de izquiera a derecha.
    
    * Pendiente: Si se mueve de su lugar esta no puede hacer nada.
    
    * update: comandos actualizados
    
    - La rotación se le encargó a al joystick izquierdo solamente.
    Pendiente: todo lo demás.
    
    * update: un monton
    
    - Se queda quieto en cabina.
    - Se rota junto con la silla (voltear usando joystick derecho)
    - Se puede interactuar con ambos Joystick virtuales.
    -Joystick izquierdo mueve brazo princilpa... Fala: evitar que se acumulen los angulos de inclinasion; meora en los limites de rotacion.
    Joystick derecho: interactua con segunda mitad del brazo de la retroexcavadora.... Falta: que rote en Y de manera eficiente.
    - Pala del brazo: No se hizo nada.
    
    ---------
    
    Co-authored-by: Leonardo Emilio Bustos Vallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>

---
+++ Assets/ProyectoRetroExcabadora/mScripts/BrazoControlSecundario.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrazoControlSecundario : MonoBehaviour
+{
+    public Transform brazo;         // El objeto que rota en Y (segundo brazo)
+    public Transform palancaVR;     // Joystick derecho
+
+    public float sensibilidadY = 1f;
+    public float anguloMaxY = 30f, anguloMinY = -75f;
+
+    private bool estaAgarrado = false;
+    private float tiempoInicioAgarrado = 0f;
+    private float tiempoIgnorar = 0.5f;
+
+    private Quaternion rotacionInicialPalanca;
+    private Quaternion rotacionBase;
+    private bool referenciaAjustada = false;
+
+    public void EmpezarAgarrar()
+    {
+        estaAgarrado = true;
+        tiempoInicioAgarrado = Time.time;
+        referenciaAjustada = false;
+        Debug.Log("Joystick derecho agarrado");
+    }
+
+    public void Soltar()
+    {
+        estaAgarrado = false;
+        Debug.Log("Joystick derecho soltado");
+    }
+
+    void Update()
+    {
+        if (!estaAgarrado) return;
+        if (Time.time - tiempoInicioAgarrado < tiempoIgnorar) return;
+
+        if (!referenciaAjustada)
+        {
+            rotacionInicialPalanca = palancaVR.localRotation;
+            rotacionBase = brazo.localRotation;
+            referenciaAjustada = true;
+            return;
+        }
+
+        float deltaY = palancaVR.localEulerAngles.y - rotacionInicialPalanca.eulerAngles.y;
+        if (deltaY > 180f) deltaY -= 360f;
+
+        float anguloY = Mathf.Clamp(deltaY * sensibilidadY, anguloMinY, anguloMaxY);
+
+        brazo.localRotation = rotacionBase * Quaternion.Euler(0, anguloY, 0);
+
+        Debug.Log($"ΔY: {deltaY:F2} | rotY: {anguloY:F2}");
+    }
+}
--- Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
+++ Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,48 +9,52 @@ public class RetroExcabadora : MonoBehaviour
     public GameObject player;
     public GameObject targetInVehicle;
     public GameObject playerCamera;
-    //public CharacterController characterController;
-    bool isInVehicle = false;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Transform puntoMira;
+    public Transform puntoSalida;
+    public float cooldownTime = 1.0f;
 
-    }
+    private bool isInVehicle = false;
+    private float lastToggleTime = -999f;
 
-    // Update is called once per frame
+    public static bool EnCabina { get; private set; } = false;
+
     void Update()
     {
-        //TODO: Solo activar cuando se toque la puerta
-        if (rightAction.action.ReadValue<float>() == 1)
+        if (rightAction.action.ReadValue<float>() == 1 && Time.time - lastToggleTime > cooldownTime)
         {
-            this.EnterExitVehicle();
-            Debug.Log("Hello World");
+            lastToggleTime = Time.time;
+            EnterExitVehicle();
         }
-
     }
-    void EnterExitVehicle() {
+
+    void EnterExitVehicle()
+    {
         if (isInVehicle)
         {
-            Debug.Log("Exit the vehicle");
-            //characterController.height = 5;
+            // Salir de la cabina
+            player.transform.position = puntoSalida.transform.position;
+            EnCabina = false;
+            isInVehicle = false;
         }
         else
         {
-            Debug.Log("Enter the vehicle");
-            //characterController.height = 2;
-            //playerCamera.transform.Rotate(7.9f, -142.6f, 0f);
-            //playerCamera.transform.rotation = Quaternion.Euler(7.9f, -142.6f, 0f);
-            //player.transform.Rotate(-3.847f, 374.079f, 0f, Space.World);
-            //playerCamera.transform.Rotate(0f, 0f, 0f, Space.World);
+            // Entrar a la cabina
             player.transform.position = targetInVehicle.transform.position + new Vector3(0f, -0.5f, 0f);
 
+            Vector3 direccionObjetivo = puntoMira.forward;
+            direccionObjetivo.y = 0;
+
+            Vector3 direccionCamara = playerCamera.transform.forward;
+            direccionCamara.y = 0;
+
+            Quaternion rotacionActual = Quaternion.LookRotation(direccionCamara);
+            Quaternion rotacionDeseada = Quaternion.LookRotation(direccionObjetivo);
+            Quaternion ajusteRotacion = rotacionDeseada * Quaternion.Inverse(rotacionActual);
+
+            player.transform.rotation = ajusteRotacion * player.transform.rotation;
+
+            EnCabina = true;
+            isInVehicle = true;
         }
     }
 }
-//-3.847
-//374.079
-
-
-
-//7.8
-//-142.6
