c6cdb9542c9a53b7931ed60c5bf5d936bbb868c0
MartinCastroMorales-mcm <mcm.martincastromorales@gmail.com>
2025-06-24 18:02:06 -0400

    cambios puerta

--- Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
+++ Assets/ProyectoRetroExcabadora/mScripts/RetroExcabadora.cs
@@ -6,11 +6,17 @@ using UnityEngine.InputSystem;
 public class RetroExcabadora : MonoBehaviour
 {
     public InputActionProperty rightAction;
+    public InputActionProperty leftAction;
     public GameObject player;
     public GameObject targetInVehicle;
     public GameObject playerCamera;
+    public GameObject DoorCube;
+
+    public GameObject LeftHand;
+    public GameObject RightHand;
     //public CharacterController characterController;
-    bool isInVehicle = false;
+    private bool isInVehicle = false;
+    private bool isDoorCubeVisible = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,37 +26,58 @@ public class RetroExcabadora : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //TODO: Solo activar cuando se toque la puerta
-        if (rightAction.action.ReadValue<float>() == 1)
-        {
-            this.EnterExitVehicle();
-            Debug.Log("Hello World");
-        }
-
+        InputHandlingForDoor();
     }
+
+    void InputHandlingForDoor()
+    {
+        //if either hand is in, the block turns yellow and the hand thats inside can "pinch it" to enter
+        Bounds cubeBounds = new Bounds(DoorCube.transform.position, DoorCube.transform.localScale);
+        Vector3 rightHandPosition = RightHand.transform.position;
+        Vector3 leftHandPosition = LeftHand.transform.position;
+        if (cubeBounds.Contains(rightHandPosition)) {
+            //turn block yellow
+            setDoorCubeVisible(true);
+            //check if button is pressed
+            if (rightAction.action.ReadValue<float>() == 1)
+            {
+                this.EnterExitVehicle();
+            }
+            return;
+        }
+        if (cubeBounds.Contains(leftHandPosition)) {
+            //turn block yellow
+            setDoorCubeVisible(true);
+            //check if button is pressed
+            if (leftAction.action.ReadValue<float>() == 1)
+            {
+                this.EnterExitVehicle();
+            }
+            return;
+        }
+        setDoorCubeVisible(false);
+    }
+
+    void setDoorCubeVisible(bool willDoorVisible)
+    {
+        DoorCube.GetComponent<MeshRenderer>().enabled = willDoorVisible;
+    }
+
     void EnterExitVehicle() {
         if (isInVehicle)
         {
             Debug.Log("Exit the vehicle");
-            //characterController.height = 5;
         }
         else
         {
             Debug.Log("Enter the vehicle");
-            //characterController.height = 2;
-            //playerCamera.transform.Rotate(7.9f, -142.6f, 0f);
-            //playerCamera.transform.rotation = Quaternion.Euler(7.9f, -142.6f, 0f);
-            //player.transform.Rotate(-3.847f, 374.079f, 0f, Space.World);
-            //playerCamera.transform.Rotate(0f, 0f, 0f, Space.World);
             player.transform.position = targetInVehicle.transform.position + new Vector3(0f, -0.5f, 0f);
-
         }
     }
 }
+
+//add to press the button of the hand in tthe blob
 //-3.847
 //374.079
-
-
-
 //7.8
 //-142.6
